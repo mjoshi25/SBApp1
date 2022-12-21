f6ea9bb437fc3d14342f04fb2329589ebaeb062a
mjoshi25 <manabhanjan22@gmail.com>
2022-12-21 12:03:46 +0530

    Initial commit

---
+++ src/main/java/com/joshi/SBApp1/TestSbApp1Application.java
@@ -0,0 +1,13 @@
+package com.joshi.SBApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSbApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSbApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshi/SBApp1/TestSbApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshi.SBApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSbApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
